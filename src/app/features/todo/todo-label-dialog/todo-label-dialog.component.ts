43cc327e90b726daf7658e65b79474f2eb6d8840
techyaura <techyaura@gmail.com>
2021-05-23 02:59:36 +0530

    Add color picker

--- src/app/features/todo/todo-label-dialog/todo-label-dialog.component.ts
+++ src/app/features/todo/todo-label-dialog/todo-label-dialog.component.ts
@@ -44,17 +44,6 @@ export class TodoLabelDialogComponent implements OnInit {
     }
   }
 
-  refreshColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    this.formObj.patchValue({
-      color: this.generateColor()
-    });
-  }
-
   generateColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
