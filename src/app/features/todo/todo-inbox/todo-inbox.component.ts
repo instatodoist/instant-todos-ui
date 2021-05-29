624a8f37d265814b42a85059ac25f1f4c9253dcf
techyaura <techyaura@gmail.com>
2021-05-30 04:04:14 +0530

    refactor code

--- src/app/features/todo/todo-inbox/todo-inbox.component.ts
+++ src/app/features/todo/todo-inbox/todo-inbox.component.ts
@@ -86,36 +86,138 @@ export class TodoInboxComponent implements OnInit, AfterViewInit, OnDestroy {
   ) {
   }
 
+  ngOnInit(): void {
+    this.fetchTodosOnLoad();
+  }
+
+  ngAfterViewInit(): void { }
+
   ngOnDestroy(): void {
     // unsubscript complete
-    if(this.fetchCompletedTodosSubscription){
+    if (this.fetchCompletedTodosSubscription) {
       this.fetchCompletedTodosSubscription.unsubscribe();
     }
     // unsubscribe delete
-    if(this.deleteReqSubscription){
+    if (this.deleteReqSubscription) {
       this.deleteReqSubscription.unsubscribe();
     }
     this.fetchTodosSubscription.unsubscribe();
   }
 
-  ngAfterViewInit(): void {}
-
-  ngOnInit(): void {
-    this.fetchTodosOnLoad();
+  /**
+   * @param todo - todo object
+   * @param popupType - update/delete
+   */
+  openPopUp(todo: TodoType): void {
+    const modelRef = this.modalService.open(TodoDialogComponent, {
+      size: 'lg',
+      scrollable: true
+    });
+    modelRef.componentInstance.todo = todo;
   }
 
-  fetchTodosOnLoad(){
+  /**
+   * @param todo - todo object
+   */
+  updateTodo(todo: TodoType): void {
+    this.todo = { ...todo };
+    const postBody: TodoType = {
+      // eslint-disable-next-line no-underscore-dangle
+      _id: this.todo._id,
+      isCompleted: !this.todo.isCompleted,
+    };
+    const { _id: id, ...body } = postBody;
+    this.todoService
+      .updateTodo(id, body, this.conditions)
+      .subscribe(() => {
+        this.todo = null;
+        // navigate to today route if no pending task
+        if (this.todoCurrentType === this.TODOTYPES.pending && !this.count.pending) {
+          this.router.navigate(['/tasks/today']);
+        }
+      });
+  }
+
+  /**
+   * @param todo - todo object
+   */
+  deleteTodo(todo: TodoType): void {
+    if (todo.deleteRequest) {
+      this.isDeleting = true;
+      const { _id: id, ..._ } = todo;
+      this.deleteReqSubscription = this.todoService
+        .deleteTodo(id, this.conditions)
+        .subscribe(() => {
+          this.isDeleting = false;
+          this.toastr.toastrSuccess('Task Deleted');
+        });
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @HostListener('scroll', ['$event'])
+  refresh(): void {
+    const { totalCount, data } = this.todosC;
+    // if (this.conditions.offset === ) {
+    //   this.getCompletedTodos(this.conditions);
+    // } else
+    if (data.length < totalCount && this.loader) {
+      const { offset } = this.conditions;
+      this.conditions = { ...this.conditions, offset: offset + 1 };
+      this.getCompletedTodos(this.conditions);
+    }
+  }
+
+  get trackIds(): string[] {
+    // eslint-disable-next-line no-underscore-dangle
+    return this.todos.data.map(track => track._id);
+  }
+
+  onTodoDrop(event: CdkDragDrop<TodoType[]>): void {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  }
+
+  private customizeCompleteTodos(response: any, isDirectCall = false): void {
+    const { totalCount, data = [] } = response;
+    if (isDirectCall) {
+      this.todosC = { ...this.todosC, totalCount, data };
+    } else {
+      this.todosC = { totalCount, data: [...this.todosC.data, ...data] };
+    }
+    if ((totalCount === null) || (data === null) || (data.length === totalCount)) {
+      this.loader = false;
+    } else {
+      this.loader = true;
+    }
+  }
+
+  /**
+   * @param conditions - based on route
+   */
+  private getCompletedTodos(conditions: TodoConditions, isDirectCall = false): void {
+    this.loader = true;
+    if (isDirectCall) {
+      conditions.offset = 1;
+    }
+    this.extraLoader = false;
+    this.fetchCompletedTodosSubscription = this.todoService.fetchCompleted(conditions)
+      .subscribe(({ data }) => {
+        this.customizeCompleteTodos(data.todoCompleted);
+      });
+  }
+
+  private fetchTodosOnLoad() {
     this.todoCurrentType = ''; // default to inbox
     this.loader = true;
     this.fetchTodosSubscription = this.projectService.fetchAll()
       .pipe(
-        switchMap(data=> combineLatest([of(data), this.activatedRoute.params, this.activatedRoute.queryParams])),
+        switchMap(data => combineLatest([of(data), this.activatedRoute.params, this.activatedRoute.queryParams])),
         map(data => ({
           labels: data[0],
           params: data[1],
           query: data[2]
         })),
-        switchMap(data=>{
+        switchMap(data => {
           const { params = null, query = null, labels } = data;
           const { label = null } = params;
           const { q = '' } = query;
@@ -162,14 +264,14 @@ export class TodoInboxComponent implements OnInit, AfterViewInit, OnDestroy {
       .subscribe(response => {
         const [countObj, dataList] = response;
         const { data = {} } = dataList;
-        if(data.hasOwnProperty('todoList')){
-          const todos = {...data.todoList};
+        if (data.hasOwnProperty('todoList')) {
+          const todos = { ...data.todoList };
           this.todos = {
             ...this.todos,
             totalCount: todos.totalCount,
             data: this.todoService.sortArrayByDate(todos.data, 'createdAt')
           };
-        } else if(data.hasOwnProperty('todoCompleted')){
+        } else if (data.hasOwnProperty('todoCompleted')) {
           this.customizeCompleteTodos(data.todoCompleted, true);
         }
         const { today = 0, pending = 0, inbox = 0, completed = 0, upcoming = 0 } = countObj;
@@ -185,108 +287,6 @@ export class TodoInboxComponent implements OnInit, AfterViewInit, OnDestroy {
           this.appService.configureSeo(this.todoCurrentType);
         }
       });
-  }
-
-  customizeCompleteTodos(response: any, isDirectCall = false): void {
-    const { totalCount, data = [] } = response;
-    if (isDirectCall) {
-      this.todosC = { ...this.todosC, totalCount, data };
-    } else {
-      this.todosC = { totalCount, data: [...this.todosC.data, ...data] };
-    }
-    if ((totalCount === null) || (data === null) || (data.length === totalCount)) {
-      this.loader = false;
-    } else {
-      this.loader = true;
-    }
-  }
-
-  /**
-   * @param conditions - based on route
-   */
-   getCompletedTodos(conditions: TodoConditions, isDirectCall = false): void {
-    this.loader = true;
-    if (isDirectCall) {
-      conditions.offset = 1;
-    }
-    this.extraLoader = false;
-    this.fetchCompletedTodosSubscription = this.todoService.fetchCompleted(conditions)
-      .subscribe(({data}) => {
-        this.customizeCompleteTodos(data.todoCompleted);
-      });
-  }
-
-  /**
-   * @param todo - todo object
-   * @param popupType - update/delete
-   */
-  openPopUp(todo: TodoType): void {
-    const modelRef  = this.modalService.open(TodoDialogComponent, {
-      size: 'lg',
-      scrollable: true
-    });
-    modelRef.componentInstance.todo = todo;
-  }
-
-  /**
-   * @param todo - todo object
-   */
-   updateTodo(todo: TodoType): void {
-    this.todo = { ...todo };
-    const postBody: TodoType = {
-      // eslint-disable-next-line no-underscore-dangle
-      _id: this.todo._id,
-      isCompleted: !this.todo.isCompleted,
-    };
-    const {_id: id, ...body} = postBody;
-    this.todoService
-      .updateTodo(id, body, this.conditions)
-      .subscribe(() => {
-        this.todo = null;
-        // navigate to today route if no pending task
-        if (this.todoCurrentType === this.TODOTYPES.pending && !this.count.pending) {
-          this.router.navigate(['/tasks/today']);
-        }
-      });
-  }
-
-  /**
-   * @param todo - todo object
-   */
-  deleteTodo(todo: TodoType): void {
-    if (todo.deleteRequest) {
-      this.isDeleting = true;
-      const { _id: id, ..._} = todo;
-      this.deleteReqSubscription = this.todoService
-        .deleteTodo(id, this.conditions)
-        .subscribe(() => {
-          this.isDeleting = false;
-          this.toastr.toastrSuccess('Task Deleted');
-        });
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  @HostListener('scroll', ['$event'])
-  refresh(): void {
-    const { totalCount, data } = this.todosC;
-    // if (this.conditions.offset === ) {
-    //   this.getCompletedTodos(this.conditions);
-    // } else
-    if (data.length < totalCount && this.loader) {
-      const { offset } = this.conditions;
-      this.conditions = { ...this.conditions, offset: offset + 1 };
-      this.getCompletedTodos(this.conditions);
-    }
-  }
-
-  get trackIds(): string[] {
-    // eslint-disable-next-line no-underscore-dangle
-    return this.todos.data.map(track => track._id);
-  }
-
-  onTodoDrop(event: CdkDragDrop<TodoType[]>): void {
-    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
   }
 
 }
